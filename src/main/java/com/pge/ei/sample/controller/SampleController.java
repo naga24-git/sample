e9150a0a37e6588dfa7e17514e8448a147cfe9c6
Nembi Sekar <GXNU@pge.com>
2018-02-23 16:30:11 +0530

    Integrated Swagger;

--- src/main/java/com/pge/ei/sample/controller/SampleController.java
+++ src/main/java/com/pge/ei/sample/controller/SampleController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pge.ei.sample.service.SampleService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class SampleController {
 	
@@ -16,6 +18,7 @@ private static final Logger LOGGER = Logger.getLogger(SampleController.class);
 	@Autowired
 	private SampleService sampleService;
 	
+	@ApiOperation(value = "Sample Controller")
 	@RequestMapping(method = RequestMethod.GET, value = "/sampleMethod")
 	public void sampleMethod() {
 		LOGGER.info("Calling sample service...");
--- src/main/java/com/pge/ei/sample/model/Employee.java
+++ src/main/java/com/pge/ei/sample/model/Employee.java
@@ -2,8 +2,11 @@ package com.pge.ei.sample.model;
 
 import com.pge.ei.sample.aspect.Loggable;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Employee {
 
+	@ApiModelProperty(notes = "Name of the employee")
 	private String name;
 	
 	public String getName() {
